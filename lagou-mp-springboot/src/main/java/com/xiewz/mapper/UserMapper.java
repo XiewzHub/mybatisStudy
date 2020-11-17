49acb712dec060b45a906dafd27be6019057378a
Xiewz <673275656@qq.com>
2020-11-18 01:23:26 +0800

    mp+springboot整合成功

--- lagou-mp-springboot/src/main/java/com/xiewz/mapper/UserMapper.java
+++ lagou-mp-springboot/src/main/java/com/xiewz/mapper/UserMapper.java
@@ -1,8 +1,13 @@
 package com.xiewz.mapper;
 
 //import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.mapper.BaseMapper;
+//import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xiewz.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
+@Component
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
--- lagou-mp-springboot/src/test/java/com/xiewz/lagoumpspringboot/LagouMpSpringbootApplicationTests.java
+++ lagou-mp-springboot/src/test/java/com/xiewz/lagoumpspringboot/LagouMpSpringbootApplicationTests.java
@@ -1,5 +1,10 @@
 package com.xiewz.lagoumpspringboot;
 
+//import com.baomidou.mybatisplus.mapper.EntityWrapper;
+//import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xiewz.mapper.UserMapper;
 import com.xiewz.pojo.User;
 import org.junit.Test;
@@ -23,6 +28,25 @@ public class LagouMpSpringbootApplicationTests {
 		for (User user : userList) {
 			System.out.println(user);
 		}
+	}
+	@Test
+	public void testSelectById() {
+		User user1 = userMapper.selectById(1);
+		System.out.println(user1);
+	}
+
+	@Test
+	public void testSelectPage() {
+
+		Page<User> userPage = new Page<>(1, 3);
+		IPage<User> userIPage = userMapper.selectPage(userPage, new QueryWrapper<User>().gt("age", 18));
+//		for (User user : userList) {
+//			System.out.println(user);
+//		}
+		System.out.println();
+		System.out.println("总数："+userIPage.getTotal());
+		System.out.println("页数："+userIPage.getSize());
+		System.out.println("当前页数据："+userIPage.getRecords());
 	}
 
 }
