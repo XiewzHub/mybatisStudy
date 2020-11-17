a25f9ac395f831d6ecf6a20cde04a7157fae431a
Xiewz <673275656@qq.com>
2020-11-18 02:14:59 +0800

    alleq使用方法

--- lagou-mp-springboot/src/test/java/com/xiewz/lagoumpspringboot/LagouMpSpringbootApplicationTests.java
+++ lagou-mp-springboot/src/test/java/com/xiewz/lagoumpspringboot/LagouMpSpringbootApplicationTests.java
@@ -12,8 +12,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -68,6 +72,29 @@ public class LagouMpSpringbootApplicationTests {
 		System.out.println("总数："+userIPage.getTotal());
 		System.out.println("页数："+userIPage.getSize());
 		System.out.println("当前页数据："+userIPage.getRecords());
+	}
+
+	/**
+	 * 测试条件构建器
+	 */
+	@Test
+	public void testAlleq(){
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("name","Sandy");
+		map.put("age",null);
+		QueryWrapper<User> queryWrapper = new QueryWrapper<User>();
+		// and链接，全部查询
+//		queryWrapper.allEq(map);
+		// 去掉name条件
+		queryWrapper.allEq((k,v)->!"name".equals(k),map);
+
+		List<User> users = userMapper.selectList(queryWrapper);
+		System.out.println("\n查询结果是否为空：");
+		System.out.println(CollectionUtils.isEmpty(users));
+		for (User user : users) {
+			System.out.println();
+			System.out.println(user);
+		}
 	}
 
 }
